syxunion/valgrind-loongarch64

loongarch64: Implement chainXDirect_LOONGARCH64()/unchainXDirect_LOONGARCH64()

VEX/priv/host_loongarch64_defs.c
@@ -2621,7 +2621,31 @@ VexInvalRange chainXDirect_LOONGARCH64 ( VexEndness endness_host,
 {
    vassert(endness_host == VexEndnessLE);
 
-   VexInvalRange vir = { (HWord)place_to_chain, 0 };
+   /* What we're expecting to see is:
+    *  la $t0, disp_cp_chain_me_to_EXPECTED
+    *  jirl $ra, $t0, 0
+    * viz
+    *  <16 bytes generated by mkLoadImm>
+    *  jirl $ra, $t0, 0
+    */
+   UInt* p = (UInt*)place_to_chain;
+   vassert(((HWord)p & 3) == 0);
+   vassert(is_LoadImm(p, hregT0(), (ULong)(Addr)disp_cp_chain_me_EXPECTED));
+   vassert(p[4] == emit_op_offs16_rj_rd(LAextra_JIRL, 0, 12, 1));
+
+   /* And what we want to change it to is:
+    *  la $t0, place_to_jump_to
+    *  jirl $ra, $t0, 0
+    * viz
+    *  <16 bytes generated by mkLoadImm>
+    *  jirl $ra, $t0, 0
+    *
+    * The replacement has the same length as the original.
+    */
+   p = mkLoadImm(p, hregT0(), (ULong)(Addr)place_to_jump_to);
+   *p++ = emit_op_offs16_rj_rd(LAextra_JIRL, 0, 12, 1);
+
+   VexInvalRange vir = { (HWord)place_to_chain, 4 * 4 + 4 };
    return vir;
 }
 
@@ -2634,7 +2658,31 @@ VexInvalRange unchainXDirect_LOONGARCH64 ( VexEndness endness_host,
 {
    vassert(endness_host == VexEndnessLE);
 
-   VexInvalRange vir = { (HWord)place_to_unchain, 0 };
+   /* What we're expecting to see is:
+    *  la $t0, place_to_jump_to_EXPECTED
+    *  jirl $ra, $t0, 0
+    * viz
+    *  <16 bytes generated by mkLoadImm>
+    *  jirl $ra, $t0, 0
+    */
+   UInt* p = (UInt*)place_to_unchain;
+   vassert(((HWord)p & 3) == 0);
+   vassert(is_LoadImm(p, hregT0(), (ULong)(Addr)place_to_jump_to_EXPECTED));
+   vassert(p[4] == emit_op_offs16_rj_rd(LAextra_JIRL, 0, 12, 1));
+
+   /* And what we want to change it to is:
+    *  la $t0, disp_cp_chain_me
+    *  jirl $ra, $t0, 0
+    * viz
+    *  <16 bytes generated by mkLoadImm>
+    *  jirl $ra, $t0, 0
+    *
+    * The replacement has the same length as the original.
+    */
+   p = mkLoadImm(p, hregT0(), (ULong)(Addr)disp_cp_chain_me);
+   *p++ = emit_op_offs16_rj_rd(LAextra_JIRL, 0, 12, 1);
+
+   VexInvalRange vir = { (HWord)place_to_unchain, 4 * 4 + 4 };
    return vir;
 }
 

Loongson-Cloud-Community/valgrind

VEX/priv/host_loongarch64_defs.c
@@ -2890,7 +2890,31 @@ VexInvalRange chainXDirect_LOONGARCH64 ( VexEndness endness_host,
 {
    vassert(endness_host == VexEndnessLE);
 
-   VexInvalRange vir = { (HWord)place_to_chain, 0 };
+   /* What we're expecting to see is:
+    *  la $t0, disp_cp_chain_me_to_EXPECTED
+    *  jirl $ra, $t0, 0
+    * viz
+    *  <16 bytes generated by mkLoadImm_EXACTLY4>
+    *  jirl $ra, $t0, 0
+    */
+   UInt* p = (UInt*)place_to_chain;
+   vassert(((HWord)p & 3) == 0);
+   vassert(is_LoadImm_EXACTLY4(p, hregT0(), (ULong)(Addr)disp_cp_chain_me_EXPECTED));
+   vassert(p[4] == emit_op_offs16_rj_rd(LAextra_JIRL, 0, 12, 1));
+
+   /* And what we want to change it to is:
+    *  la $t0, place_to_jump_to
+    *  jirl $ra, $t0, 0
+    * viz
+    *  <16 bytes generated by mkLoadImm_EXACTLY4>
+    *  jirl $ra, $t0, 0
+    *
+    * The replacement has the same length as the original.
+    */
+   p = mkLoadImm_EXACTLY4(p, hregT0(), (ULong)(Addr)place_to_jump_to);
+   *p++ = emit_op_offs16_rj_rd(LAextra_JIRL, 0, 12, 1);
+
+   VexInvalRange vir = { (HWord)place_to_chain, 4 * 4 + 4 };
    return vir;
 }
 
@@ -2903,7 +2927,31 @@ VexInvalRange unchainXDirect_LOONGARCH64 ( VexEndness endness_host,
 {
    vassert(endness_host == VexEndnessLE);
 
-   VexInvalRange vir = { (HWord)place_to_unchain, 0 };
+   /* What we're expecting to see is:
+    *  la $t0, place_to_jump_to_EXPECTED
+    *  jirl $ra, $t0, 0
+    * viz
+    *  <16 bytes generated by mkLoadImm_EXACTLY4>
+    *  jirl $ra, $t0, 0
+    */
+   UInt* p = (UInt*)place_to_unchain;
+   vassert(((HWord)p & 3) == 0);
+   vassert(is_LoadImm_EXACTLY4(p, hregT0(), (ULong)(Addr)place_to_jump_to_EXPECTED));
+   vassert(p[4] == emit_op_offs16_rj_rd(LAextra_JIRL, 0, 12, 1));
+
+   /* And what we want to change it to is:
+    *  la $t0, disp_cp_chain_me
+    *  jirl $ra, $t0, 0
+    * viz
+    *  <16 bytes generated by mkLoadImm_EXACTLY4>
+    *  jirl $ra, $t0, 0
+    *
+    * The replacement has the same length as the original.
+    */
+   p = mkLoadImm_EXACTLY4(p, hregT0(), (ULong)(Addr)disp_cp_chain_me);
+   *p++ = emit_op_offs16_rj_rd(LAextra_JIRL, 0, 12, 1);
+
+   VexInvalRange vir = { (HWord)place_to_unchain, 4 * 4 + 4 };
    return vir;
 }
 